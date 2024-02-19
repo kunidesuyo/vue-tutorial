50643035994e26556e85b0a6693821c6e6fa8b5b
kuni <kunidesuyo1234@gmail.com>
2024-02-20 07:29:54 +0900

    途中

---
+++ directive-bind-basic/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import {ref} from "vue";
+const url = ref("https://vuejs.org/");
+</script>
+
+<template>
+  <p><a v-bind:href="url" target="_blank">Vue.jsのサイト</a> </p>
+  <p
+</template>
