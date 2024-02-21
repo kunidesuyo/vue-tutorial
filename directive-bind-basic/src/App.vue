d5ef363fbaac6624a382f73f6305219b22f146e0
kuni <kunidesuyo1234@gmail.com>
2024-02-21 10:14:37 +0900

    4.1.2

--- directive-bind-basic/src/App.vue
+++ directive-bind-basic/src/App.vue
@@ -5,5 +5,6 @@ const url = ref("https://vuejs.org/");
 
 <template>
   <p><a v-bind:href="url" target="_blank">Vue.jsのサイト</a> </p>
-  <p
+  <p><a :href="url" target="_blank">Vue.jsのサイト(省略形)</a></p>
+  <p> <a v-bind:href="url + 'guide/introduction.html'" target="_blank">Vue.jsガイドのページ</a> </p>
 </template>
