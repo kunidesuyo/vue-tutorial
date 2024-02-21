9fdc77494dc4218308d8851f555e537cba26f9ec
kuni <kunidesuyo1234@gmail.com>
2024-02-21 10:19:46 +0900

    4.1.3

---
+++ directive-bind-boolean/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const isSendButtonDisabled = ref(false);
+</script>
+
+<template>
+  <p><button type="button" v-bind:disabled="isSendButtonDisabled">送信</button> </p>
+
+</template>
