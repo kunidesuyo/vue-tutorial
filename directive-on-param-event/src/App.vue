71f7d9c0f371da3cb6317534032280a69da2e351
kuni <kunidesuyo1234@gmail.com>
2024-02-23 23:31:48 +0900

    4.2.3

---
+++ directive-on-param-event/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const mousePointerX = ref(0);
+const mousePointerY = ref(0);
+const onImgMousemove = (event: MouseEvent): void => {
+  mousePointerX.value = event.offsetX;
+  mousePointerY.value = event.offsetY;
+};
+
+
+</script>
+
+<template>
+  <section>
+    <img src="./assets/logo.svg" alt="Vue log" width="200" v-on:mousemove="onImgMousemove">
+    <p>pointer position: x={{ mousePointerX }}; y={{ mousePointerY }} </p>
+  </section>
+</template>
