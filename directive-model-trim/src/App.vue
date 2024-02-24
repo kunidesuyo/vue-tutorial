2534e4c7dbc3320ed42922e0aeaba2a9e19df470
kuni <kunidesuyo1234@gmail.com>
2024-02-24 20:05:33 +0900

    5.2.1

---
+++ directive-model-trim/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import {ref} from "vue";
+const trimedText = ref("");
+</script>
+
+<template>
+  <section>
+    <input type="text" v-model.trim="trimedText">
+    <p>入力文字列: {{ trimedText }}</p>
+  </section>
+</template>
