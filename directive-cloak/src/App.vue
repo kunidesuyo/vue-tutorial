7b940d25cfa3fe4002cdb72f681471a9209a423e
kuni <kunidesuyo1234@gmail.com>
2024-02-24 20:28:48 +0900

    5.2.4

---
+++ directive-cloak/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const hello = ref("hello");
+</script>
+
+<template>
+  <p v-cloak>{{ hello }}</p>
+</template>
+
+<style>
+[v-cloak] {
+  display: none;
+}
+</style>
