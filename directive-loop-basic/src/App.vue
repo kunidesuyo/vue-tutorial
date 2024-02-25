ef68a951022ca91b064f127f4d817783ff9397d4
kuni <kunidesuyo1234@gmail.com>
2024-02-25 23:36:39 +0900

    6.2.1

---
+++ directive-loop-basic/src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const cocktailListInit: string[] = ["white lady", "blue hawaii", "newyork"];
+const cocktailList = ref(cocktailListInit);
+</script>
+
+<template>
+  <ul>
+    <li
+      v-for="cocktailName in cocktailList"
+      v-bind:key="cocktailName">
+      {{ cocktailName }}
+    </li>
+  </ul>
+  <ul>
+    <li
+      v-for="(cocktailName, index) in cocktailList"
+      v-bind:key="cocktailName">
+      {{ cocktailName }}(index {{ index }})
+    </li>
+  </ul>
+</template>
