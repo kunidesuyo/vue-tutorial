5b6239afcc442c45a27dadc21b9105de720285f9
kuni <kunidesuyo1234@gmail.com>
2024-02-25 23:58:19 +0900

    6.2.5

---
+++ directive-loop-obj/src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import {ref} from "vue";
+const whiteLadyInit: {
+  id: number;
+  name: string;
+  price: number;
+  recipe: string;
+} = {
+  id: 2345,
+  name: "white lady",
+  price: 1200,
+  recipe: "jin 30ml + cowantoro 15ml + lemon juice 15ml"
+};
+const whiteLady = ref(whiteLadyInit);
+</script>
+
+<template>
+  <dl>
+    <template
+      v-for="(value, key) in whiteLady"
+      v-bind:key="key">
+      <dt>{{ key }}</dt>
+      <dd>{{ value }}</dd>
+    </template>
+  </dl>
+</template>
