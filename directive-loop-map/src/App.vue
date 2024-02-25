29f2cda67cb8eac109564067dc33c1e702313266
kuni <kunidesuyo1234@gmail.com>
2024-02-25 23:51:19 +0900

    6.2.3

---
+++ directive-loop-map/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const cocktailListInit = new Map<number, string>();
+cocktailListInit.set(2345, "white lady");
+cocktailListInit.set(4412, "blue hawaii");
+cocktailListInit.set(6792, "newyork");
+const cocktailList = ref(cocktailListInit);
+</script>
+
+<template>
+  <ul>
+    <li
+      v-for="[id, cocktailName] in cocktailList"
+      v-bind:key="id">
+      IDが{{ id }}のカクテルは{{ cocktailName }}
+    </li>
+  </ul>
+</template>
