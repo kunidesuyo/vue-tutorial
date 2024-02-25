5fbc1a47948e4537518d2adc8b8fad6b1a985bdc
kuni <kunidesuyo1234@gmail.com>
2024-02-25 22:13:10 +0900

    6.1.3

---
+++ directive-conditional-template/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import {computed} from "vue";
+
+const randomNumber = computed(
+  (): number => {
+    return Math.round(Math.random() * 100);
+  }
+);
+</script>
+
+<template>
+  <p>
+    点数は{{ randomNumber }}点で
+    <template v-if="randomNumber >= 80">
+      優です。
+      <span style="color: red;">素晴らしい！</span>
+    </template>
+  </p>
+</template>
