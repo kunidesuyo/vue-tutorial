b94246ddfa89b10ca87d196f3ce6213a03636626
kuni <kunidesuyo1234@gmail.com>
2024-02-29 21:18:19 +0900

    chapter7

--- chapter7/src/App.vue
+++ chapter7/src/App.vue
@@ -343,7 +343,7 @@ interface Cocktail {
 </template> -->
 
 <!-- 7-10 -->
-<script lang="ts">
+<!-- <script lang="ts">
 import {defineComponent, reactive, computed, toRefs} from "vue";
 
 export default defineComponent({
@@ -377,4 +377,40 @@ export default defineComponent({
 
 <template>
 	<p>半径{{radius}}の円の面積を円周率{{PI}}で計算すると、{{area}}</p>
+</template> -->
+
+
+<!-- 7-11 -->
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+	name: "App",
+	data() {
+		return {
+			height: Math.round(Math.random() * 10),
+			width: Math.round(Math.random() * 10)
+		}
+	},
+	computed: {
+		area(): number {
+			return this.height * this.width;
+		}
+	},
+	methods: {
+		change(): void {
+			this.height = Math.round(Math.random() * 10);
+			this.width = Math.round(Math.random() * 10);
+		}
+	},
+	updated(): void {
+		console.log(`beforeMount called: ${this.height} * ${this.width}`);
+	}
+});
+</script>
+
+<template>
+	<p>縦が{{height}}、横が{{width}}の長方形の面積は{{area}}</p>
+	<button v-on:click="change">値を変更</button>
 </template>
