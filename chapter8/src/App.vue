a1dc783d91c9e5c4f9d5e6f0c5d06d4cd354f322
kuni <kunidesuyo1234@gmail.com>
2024-03-02 20:31:40 +0900

    8.5

--- chapter8/src/App.vue
+++ chapter8/src/App.vue
@@ -153,7 +153,7 @@ interface Member {
 
 <!-- 8-14 -->
 
-<script setup lang="ts">
+<!-- <script setup lang="ts">
 import {ref} from "vue";
 import OneSection2 from "./components/OneSection2.vue";
 
@@ -171,4 +171,53 @@ const onCreateNewRand = (): void => {
       v-bind:rand="rand"
       v-on:createNewRand="onCreateNewRand"/>
   </section>
+</template> -->
+
+<!-- 8-16 -->
+
+<script setup lang="ts">
+import {ref, computed} from "vue";
+import OneMember from "./components/OneMember.vue";
+
+//会員リストデータを用意。
+const memberListInit = new Map<number, Member>();
+memberListInit.set(33456, {id: 33456, name: "田中太郎", email: "bow@example.com", points: 35, note: "初回入会特典あり。"});
+memberListInit.set(47783, {id: 47783, name: "鈴木二郎", email: "mue@example.com", points: 53});
+const memberList = ref(memberListInit);
+
+//会員リスト内の全会員のポイント合計の算出プロパティ。
+const totalPoints = computed(
+	(): number => {
+		let total = 0;
+		for(const member of memberList.value.values()) {
+			total += member.points;
+		}
+		return total;
+	}
+);
+
+
+//会員情報インターフェース。
+interface Member {
+	id: number;
+	name: string;
+	email: string;
+	points: number;
+	note?: string;
+}
+</script>
+
+<template>
+	<section>
+		<h1>会員リスト</h1>
+		<p>全会員の保有ポイントの合計: {{totalPoints}}</p>
+		<OneMember
+			v-for="[id, member] in memberList"
+			v-bind:key="id"
+			v-bind:id="id"
+			v-bind:name="member.name"
+			v-bind:email="member.email"
+			v-model:points="member.points"
+			v-bind:note="member.note"/>
+	</section>
 </template>
--- chapter8/src/components/OneMember.vue
+++ chapter8/src/components/OneMember.vue
@@ -1,46 +1,64 @@
 <script setup lang="ts">
-import {ref, computed} from "vue";
+import {computed} from "vue";
 
+//Propsインターフェースの定義。
 interface Props {
-  id: number;
-  name: string;
-  email: string;
-  points: number;
-  note?: string;
+	id: number;
+	name: string;
+	email: string;
+	points: number;
+	note?: string;
 }
 
-const props = withDefaults(
-  defineProps<Props>(),
-  {note: "--"}
+//Emitインターフェースの定義。
+interface Emits {
+	(event: "update:points", points: number): void;
+}
+
+//Propsオブジェクトの設定。
+const props = defineProps<Props>();
+//Emitの設定。
+const emit = defineEmits<Emits>();
+
+//Propsのnoteを加工する算出プロパティ。
+const localNote = computed(
+	(): string => {
+		let localNote = props.note;
+		if(localNote == undefined) {
+			localNote = "--";
+		}
+		return localNote;
+	}
 );
-
-const localPoints = ref(props.points);
-
-const pointUp = (): void => {
-  localPoints.value++;
+//［ポイント加算］ボタンをクリックした時のメソッド。
+const onInput = (event: Event): void => {
+  const element = event.target as HTMLInputElement;
+  const inputPoints = Number(element.value);
+  emit("update:points", inputPoints);
 }
 </script>
 
 <template>
-  <section class="box">
-    <h4>{{ name }}さんの情報</h4>
-    <dl>
-      <dt>ID</dt>
-      <dd>{{ id }}</dd>
-      <dt>メールアドレス</dt>
-      <dd>{{ email }}</dd>
-      <dt>保有ポイント</dt>
-      <dd>{{ localPoints }}</dd>
-      <dt>備考</dt>
-      <dd>{{ note }}</dd>
-    </dl>
-    <button v-on:click="pointUp">ポイント加算</button>
-  </section>
+	<section class="box">
+		<h4>{{name}}さんの情報</h4>
+		<dl>
+			<dt>ID</dt>
+			<dd>{{id}}</dd>
+			<dt>メールアドレス</dt>
+			<dd>{{email}}</dd>
+			<dt>保有ポイント</dt>
+      <dd>
+        <input type="number" v-bind:value="points" v-on:input="onInput">
+      </dd>
+			<dt>備考</dt>
+			<dd>{{localNote}}</dd>
+		</dl>
+	</section>
 </template>
 
 <style scoped>
 .box {
-  border: green 1px solid;
-  margin: 10px;
+	border: green 1px solid;
+	margin: 10px;
 }
 </style>
