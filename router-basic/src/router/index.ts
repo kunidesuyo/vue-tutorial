431299b51fd1561498808a8b4b29532ca5ea8a28
kuni <kunidesuyo1234@gmail.com>
2024-03-03 22:33:58 +0900

    10.3

--- router-basic/src/router/index.ts
+++ router-basic/src/router/index.ts
@@ -14,7 +14,20 @@ const routeSettings: RouteRecordRaw[] = [
     component: () => {
       return import("@/views/member/MemberList.vue");
     }
-  }
+  },
+  {
+    path: "/member/detail/:id",
+    name: "MemberDetail",
+    component: () => {
+      return import("@/views/member/MemberDetail.vue");
+    },
+    props: (routes) => {
+      const idNum = Number(routes.params.id);
+      return {
+        id: idNum
+      };
+    }
+  },
 ];
 
 const router = createRouter({
