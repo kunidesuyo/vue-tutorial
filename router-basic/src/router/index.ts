ed936343bca53d65b366ea23baa1ce80072a4f12
kuni <kunidesuyo1234@gmail.com>
2024-03-04 09:32:52 +0900

    10.5.4

--- router-basic/src/router/index.ts
+++ router-basic/src/router/index.ts
@@ -13,28 +13,50 @@ const routeSettings: RouteRecordRaw[] = [
     name: "MemberList",
     component: () => {
       return import("@/views/member/MemberList.vue");
-    }
-  },
-  {
-    path: "/member/detail/:id",
-    name: "MemberDetail",
-    component: () => {
-      return import("@/views/member/MemberDetail.vue");
     },
-    props: (routes) => {
-      const idNum = Number(routes.params.id);
-      return {
-        id: idNum
-      };
-    }
+    children: [
+      {
+        path: "detail/:id",
+        name: "MemberDetail",
+        component: () => {
+          return import("@/views/member/MemberDetail.vue");
+        },
+        props: (routes) => {
+          const idNum = Number(routes.params.id);
+          return {
+            id: idNum
+          };
+        }
+      },
+      {
+        path: "add",
+        name: "MemberAdd",
+        component: () => {
+          return import("@/views/member/MemberAdd.vue");
+        },
+      }
+    ]
   },
-  {
-    path: "/member/add",
-    name: "MemberAdd",
-    component: () => {
-      return import("@/views/member/MemberAdd.vue");
-    },
-  }
+  // {
+  //   path: "/member/detail/:id",
+  //   name: "MemberDetail",
+  //   component: () => {
+  //     return import("@/views/member/MemberDetail.vue");
+  //   },
+  //   props: (routes) => {
+  //     const idNum = Number(routes.params.id);
+  //     return {
+  //       id: idNum
+  //     };
+  //   }
+  // },
+  // {
+  //   path: "/member/add",
+  //   name: "MemberAdd",
+  //   component: () => {
+  //     return import("@/views/member/MemberAdd.vue");
+  //   },
+  // }
 ];
 
 const router = createRouter({
