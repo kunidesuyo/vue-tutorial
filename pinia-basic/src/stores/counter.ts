2b71e53fe6e00b1629272aeef16ce798da94262a
kuni <kunidesuyo1234@gmail.com>
2024-03-06 10:20:07 +0900

    11.1.6

--- pinia-basic/src/stores/counter.ts
+++ pinia-basic/src/stores/counter.ts
@@ -1,12 +1,21 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
+export const useCounterStore = defineStore({
+  id: "counter",
+  state: (): State => {
+    return {
+      counter: 0
+    };
+  },
+  getters: {
+    doubleCount: (state): number => {
+      return state.counter * 2;
+    }
+  },
+  actions: {
+    incrementCount(): void {
+      this.counter++;
+    }
   }
-
-  return { count, doubleCount, increment }
-})
+});
