c124d78cc3489414f6b3a9e44963e756225f6b20
kuni <kunidesuyo1234@gmail.com>
2024-03-09 22:25:59 +0900

    chapter 12

--- async-web/src/stores/weather.ts
+++ async-web/src/stores/weather.ts
@@ -1,4 +1,5 @@
 import {defineStore} from "pinia";
+import axios from "axios";
 
 export interface City {
   name: string;
@@ -7,17 +8,25 @@ export interface City {
 
 interface State {
   cityList: Map<string, City>;
+  selectedCity: City;
+  isLoading: boolean;
+  weatherDescription: string;
 }
 
 export const useWeatherStore = defineStore({
   id: "weather",
   state: (): State => {
     return {
-      cityList: new Map<String, City>(),
+      cityList: new Map<string, City>(),
+      selectedCity: {
+        name: "",
+        q: ""
+      },
+      isLoading: true,
+      weatherDescription: ""
     };
   },
   getters: {
-
   },
   actions: {
     prepareCityList() {
@@ -37,5 +46,28 @@ export const useWeatherStore = defineStore({
         q: "Himeji"
       });
     },
+    async recieveWeatherInfo(id: string) {
+      this.selectedCity = this.cityList.get(id) as City;;
+      const weatherInfoUrl = "http://api.openweathermap.org/data/2.5/weather";
+      const params: {
+        lang: string,
+        q: string,
+        appId: string
+      } =
+      {
+        lang: "ja",
+        q: this.selectedCity.q,
+        appId: "11a7538fc51e3de1fea32319f0af5665"
+      };
+
+      const queryParams = new URLSearchParams(params);
+      const urlFull = `${weatherInfoUrl}?${queryParams}`;
+      const response = await axios.get(urlFull);
+      const weatherInfoJSON = response.data;
+      const weatherArray = weatherInfoJSON.weather;
+      const weather = weatherArray[0];
+      this.weatherDescription = weather.description;
+      this.isLoading = false;
+    }
   }
 });
