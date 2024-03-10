27197a1076eece5001ad1e978407205c6a86ba49
kuni <kunidesuyo1234@gmail.com>
2024-03-10 16:01:36 +0900

    chapter 13

--- utest-basic/src/__tests__/App.spec.ts
+++ utest-basic/src/__tests__/App.spec.ts
@@ -1,28 +1,44 @@
 import { describe, test, expect } from "vitest";
 import {mount} from "@vue/test-utils";
 import App from "@/App.vue";
+import OneMember from "@/components/OneMember.vue";
 
 describe(
   "App.vueのテスト",
   () => {
     test(
-      "初期状態（隠し領域非表示）のテスト",
+      "合計点数表示のテスト",
       () => {
-        const wrapper = mount(App);
-        const actual = wrapper.find(`[data-testid="invisible"]`).exists();
-        const expected = false;
-        expect(actual).toBe(expected);
+        const options = {
+          global: {
+            stubs: {
+              OneMember: true
+            }
+          }
+        };
+        const wrapper = mount(App, options);
+        const actualText = wrapper.text();
+        const expected = "全会員の保有ポイントの合計: 88";
+        expect(actualText).toContain(expected);
       }
     );
     test(
-      "表示ボタンをクリックした場合のテスト",
+      "Emitメソッドのテスト",
       async () => {
-        const wrapper = mount(App);
-        await wrapper.get(`[data-testid="showButton"]`).trigger("click");
-        const actual = wrapper.find(`[data-testid="invisible"]`).exists();
-        const expected = true;
-        expect(actual).toBe(expected);
+        const options = {
+          global: {
+            stubs: {
+              OneMember: true
+            }
+          }
+        };
+        const wrapper = mount(App, options);
+        const oneMemberComponent = wrapper.findComponent(OneMember);
+        await oneMemberComponent.vm.$emit("incrementPoint", 33456);
+        const actualText = wrapper.text();
+        const expected = "全会員の保有ポイントの合計: 89";
+        expect(actualText).toContain(expected)
       }
-    );
+    )
   }
 );
